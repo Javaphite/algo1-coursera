0385736dc643d1d99957ccd9f8976314aa81267a
javaphite <javaphite@gmail.com>
2019-08-25 18:54:10 +0300

    Add Permutation implementation
    Signed-off-by: javaphite <javaphite@gmail.com>

---
+++ src/main/java/com/javaphite/algo1/stacksandqueues/Permutation.java
@@ -0,0 +1,36 @@
+package com.javaphite.algo1.stacksandqueues;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Permutation {
+
+    private static final String INVALID_ARGUMENTS_MSG =
+            "Arguments should contain int k (0 <= k <= n) as first argument, followed with n strings!";
+
+    public static void main(String[] args) {
+        int k = getNumberOfStringsToPrint(args);
+
+        if (-1 == k) {
+            throw new IllegalArgumentException(INVALID_ARGUMENTS_MSG);
+        }
+
+        RandomizedQueue<String> queue = new RandomizedQueue<>();
+        for (int i = 1; i < args.length; i++) {
+            queue.enqueue(args[i]);
+        }
+
+        for (int i=0; i < k && !queue.isEmpty(); i++) {
+            StdOut.println(queue.dequeue());
+        }
+
+    }
+
+    private static int getNumberOfStringsToPrint(String[] args) {
+        try {
+            return Integer.parseInt(args[0]);
+        } catch (Exception exception) {
+            return -1;
+        }
+    }
+
+}
